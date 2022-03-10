220051f031af62d959d17354fec271517239fdb3
Cees van de Griend <cees.van.de.griend@xantara-it.nl>
2022-03-10 10:16:49 +0100

    Fix for GPG

---
+++ zsh/50-gpg.zsh
@@ -0,0 +1,8 @@
+#
+# Author:      Cees van de Griend <c.vande.griend@gmail.com>
+# Date:        2022-03-10
+# Copyright:   ©2022  C.A. van de Griend
+# Description: Configure Zsh environment
+#
+
+export GPG_TTY=$(tty)
